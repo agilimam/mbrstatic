e617f5ecbbc32d17477ef07e3e3292b2cb87e21e
agilimam <agilimamalhafizi04@gmail.com>
2023-05-17 15:55:39 +0700

    membuat program angka construvtor dan destructor.dan mengambalikan nilai dai class angka

--- destruktur/destruktur.cpp
+++ destruktur/destruktur.cpp
@@ -12,3 +12,29 @@ public:
 	void isiData();
 };
 //Definisi member Function
+angka::angka(int i) { // constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() { // Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData() {
+	for (int i = 1;i <= panjang;i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+
+void angka::isiData() {
+	for (int i = 1;i <= panjang;i++) {
+		cout << i << " = ";cin >> arr[i];
+	}
+	cout << endl;
+}
+
