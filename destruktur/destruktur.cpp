eb4ebc54d76563784e9bd2c54c04d835dd154d2d
agilimam <agilimamalhafizi04@gmail.com>
2023-05-17 13:59:56 +0700

    membuat claas angka ada variabel yang privat dan ada yang public. dan membuat class constructor

---
+++ destruktur/destruktur.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //constructor
+	~angka(); // Destructor
+	void cetakData();
+	void isiData();
+};
+//Definisi member Function
+angka::angka(int i) { // constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
