1ef35f523c6257a8c415a82a3578b42ceca3dbf2
agilimam <agilimamalhafizi04@gmail.com>
2023-05-17 15:56:31 +0700

    memberi class buku dan judul

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -3,24 +3,5 @@ using namespace std;
 class buku
 {
 	string judul;
-public:
-	buku setJudul(string judul)
-	{
-		this->judul = judul;
-		return *this; // chain function
-	}
-	string getjudul()
-	{
-		return this->judul;
-	}
 
-}bukunya;
-
-int main()
-{
-	//bukunya.setjudul("Matematika");
-	//cout << bukunya.getjudul();
-	cout << bukunya.setJudul("matematika").getjudul(); // chain function calls
-	return 0;
-}
 
